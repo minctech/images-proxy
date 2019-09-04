3e141f523642a8a46bf6a3d0e52954a62e519937
codegirl77 <rachelklinkatsis@gmail.com>
2019-09-04 16:26:32 -0700

    connected all four modules together in one proxy

--- server.js
+++ server.js
@@ -2,27 +2,17 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser');
 const morgan = require('morgan')
-var proxy = require('http-proxy-middleware');
 
 
 const app = express();
 app.use(morgan('dev'))
 
-// app.use(
-//   'api/listings/1/',
-//   proxy({ target: 'http://localhost:3001', changeOrigin: true })
-// );
-
-// app.use(
-//   '/api/listings/1/',
-//   proxy({ target: 'http://localhost:3777', changeOrigin: true })
-// );
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/listings/:listing', express.static('./public'));
-// app.use(express.static('public'));
+
 
 
 app.listen(3000, () => {
