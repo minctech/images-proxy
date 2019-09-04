6f88f1fe2e3aea06b6864d18cb8de21cac821c8b
codegirl77 <rachelklinkatsis@gmail.com>
2019-09-04 16:05:58 -0700

    put all four proxies together

--- server.js
+++ server.js
@@ -2,16 +2,27 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser');
 const morgan = require('morgan')
+var proxy = require('http-proxy-middleware');
 
 
 const app = express();
 app.use(morgan('dev'))
 
+// app.use(
+//   'api/listings/1/',
+//   proxy({ target: 'http://localhost:3001', changeOrigin: true })
+// );
+
+// app.use(
+//   '/api/listings/1/',
+//   proxy({ target: 'http://localhost:3777', changeOrigin: true })
+// );
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/public'));
+app.use('/api/listings/:listing', express.static('./public'));
+// app.use(express.static('public'));
 
 
 app.listen(3000, () => {
