6cc9646d20520fc77162b079adea9b17883c5e53
codegirl77 <rachelklinkatsis@gmail.com>
2019-09-03 19:57:49 -0700

    Removed node_module folder

--- server.js
+++ server.js
@@ -3,25 +3,10 @@ const path = require('path')
 const bodyParser = require('body-parser');
 const morgan = require('morgan')
 
-const proxy = require('http-proxy-middleware');
 
 const app = express();
 app.use(morgan('dev'))
 
-
-
-
-app.use('/api', proxy({
-    target: 'http://localhost:3777'
-}));
-
-// app.use('/', proxy({
-//     target: 'http://localhost:3210'
-// }));
-
-// app.use('/', proxy({
-//     target: 'http://localhost:3001'
-// }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
